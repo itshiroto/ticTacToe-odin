7baa433e96138deb25c0706021b2713e89bac63f
Junichirou Hiroto <rivowowor7@gmail.com>
2021-06-07 16:11:13 +0800

    M: Added a render algorithm

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-const Gameboard = (function() {
-    const square = [
-        {mark: ''}
-    ];
-
-    let board = [];
-    const createBoard = function() {
-        for (let i = 0; i < 9; i++) {
-            board.push(square);
-        }
-    }
-
-
-    return {
-        createBoard,
-        board
-    }
-})();
-
 const Player = (move) => {
     this.move = move;
 
@@ -25,3 +6,40 @@ const Player = (move) => {
     return { getMove };
 }
 
+const Gameboard = (function() {
+    const square = [
+        {mark: ''}
+    ];
+
+    let board = [];
+
+    const get = () => {
+        return board;
+    }
+    const create = function() {
+        for (let i = 0; i < 9; i++) {
+            board.push(square);
+        }
+    }
+
+    return {
+        create,
+        get,
+    }
+})();
+
+const displayController = (function() {
+    const render = () => {
+        let array = Gameboard.get();
+        let canvas = document.querySelector("#canvasGrid");
+        array.forEach((element, index) => {
+            let item = document.createElement('div');
+            item.classList.add("board-item")
+            item.dataset.index = index;
+            canvas.appendChild(item);
+        })
+    }
+    return {
+        render,
+    }
+})()
